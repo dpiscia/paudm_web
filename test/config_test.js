a77edf950549a4df9b10f28a984621400f4be3e4
Davide <davidepi79@gmail.com>
2013-09-27 10:23:36 +0200

    add create and insert table test

--- test/config_test.js
+++ test/config_test.js
@@ -1,6 +1,6 @@
 var config = {};
 
-config.job = {client : "sqlite", name : ":memory:"};
-config.pau = {client : "sqlite", name : ":memory:"};
+config.job = {client : "sqlite", name : "test/test_DB_job"};
+config.pau = {client : "sqlite", name : "test/test_DB_pau"};
 
 module.exports = config;
