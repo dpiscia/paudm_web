0f9d4341df619b315fffff024442b8b942e230b5
dpiscia <dpiscia@Dell-Vostro.(none)>
2013-10-02 21:46:58 +0200

    admin login flask

--- admin_portal_flask/app.py
+++ admin_portal_flask/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request, render_template
 from flask.ext.admin import Admin
-from flask.ext.admin.contrib.sqlamodel import ModelView
+from flask.ext.admin.contrib.sqla import ModelView
+
 from model import User, init,  recreate
 app = Flask(__name__)
 session = init('sqlite:///prova.db')
@@ -9,4 +10,26 @@ recreate()
 # Add administrative views here
 admin.add_view(ModelView(User, session))
 app.debug = True
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+    
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+    
+    
 app.run()
