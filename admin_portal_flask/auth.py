45a57073d4725fc884786745e3a18695d47d5676
Davide <davidepi79@gmail.com>
2013-10-08 08:36:35 +0200

    s

--- admin_portal_flask/auth.py
+++ admin_portal_flask/auth.py
@@ -20,30 +20,6 @@ app.config['SQLALCHEMY_ECHO'] = True
 from model import User,init, recreate
 
 
-# Create user model. For simplicity, it will store passwords in plain text.
-# Obviously that's not right thing to do in real world application.
-#class User(db.Model):
-#    id = db.Column(db.Integer, primary_key=True)
-#    login = db.Column(db.String(80), unique=True)
-#    email = db.Column(db.String(120))
-#    password = db.Column(db.String(64))
-
-#    # Flask-Login integration
-#    def is_authenticated(self):
-#        return True
-
-#    def is_active(self):
-#        return True
-
-#    def is_anonymous(self):
-#        return False
-
-#    def get_id(self):
-#        return self.id
-
-#    # Required for administrative interface
-#    def __unicode__(self):
-#        return self.username
 
 
 # Define login and registration forms (for flask-login)
